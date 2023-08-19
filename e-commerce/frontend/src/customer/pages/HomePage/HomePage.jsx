2998a0bfd53dce8ca62355129b5c11a9f07b71f7
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-08-19 18:22:28 +0300

    main carousel complated

---
+++ e-commerce/frontend/src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainCarousel from '../../components/HomeCarousel/MainCarousel'
+
+function HomePage() {
+  return (
+    <div>
+        <MainCarousel/>
+    </div>
+  )
+}
+
+export default HomePage
